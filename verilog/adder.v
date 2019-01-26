4323e88148e38d58f92079e34ad0a0983ac2b3dd
Albert Bogdanowicz <albertbogdanowicz@albertbogdanowicz.pl>
2019-01-26 12:10:08 +0100

    Decimal adder

--- verilog/adder.v
+++ verilog/adder.v
@@ -20,3 +20,27 @@ module adder8(input wire [7:0] x,
   assign s[3:0] = s0;
   assign s[7:4] = s1;
 endmodule
+
+module adder4_decimal(input wire [3:0] x,
+              input wire [3:0] y,
+              input wire c_in,
+              output wire [3:0] s,
+              output wire c_out);
+  //assign s = x + y + c_in < 10 ? x + y + c_in : x + y + c_in - 10;
+  assign s = (x + y + c_in) % 10;
+  assign c_out = (x + y + c_in) >= 10;
+endmodule
+
+module adder8_decimal(input wire [7:0] x,
+              input wire [7:0] y,
+              input wire c_in,
+              output wire [7:0] s,
+              output wire c_out);
+  wire [3:0] s0;
+  wire [3:0] s1;
+  wire c0;
+  adder4_decimal a0(x[3:0], y[3:0], c_in, s0, c0);
+  adder4_decimal a1(x[7:4], y[7:4], c0, s1, c_out);
+  assign s[3:0] = s0;
+  assign s[7:4] = s1;
+endmodule
--- verilog/adder_tb.v
+++ verilog/adder_tb.v
@@ -7,7 +7,7 @@ module Top;
   wire [7:0] s;
   wire c_out;
 
-  adder8 a(x, y, c_in, s, c_out);
+  adder8_decimal a(x, y, c_in, s, c_out);
 
   initial
     begin
@@ -15,12 +15,18 @@ module Top;
                x, y, c_in, s, c_out);
       $dumpfile(`VCD_FILE);
       $dumpvars;
-
-      x <= 'h00; y <= 'h00; c_in <= 1;    // sum = 'h01
-      #5 x <= 'h80; y <= 'h7F; c_in <= 1; // sum = 'h100
-      #5 x <= 'hA0; y <= 'h0B; c_in <= 0; // sum = 'hAB
-      #5 x <= 'h08; y <= 'h18; c_in <= 0; // sum = 'h20
-      #5 x <= 'h7F; y <= 'hA0; c_in <= 0; // sum = 'h11F
+      
+      #5 x <= 'h0; y <= 'h0; c_in <= 'h0;
+      #5 x <= 'h2; y <= 'h2; c_in <= 'h1;
+      #5 x <= 'h5; y <= 'h9; c_in <= 'h0;
+      #5 x <= 'h7; y <= 'h1; c_in <= 'h1;
+      #5 x <= 'h6; y <= 'h4; c_in <= 'h0;
+      #5 x <= 'h9; y <= 'h9; c_in <= 'h0;
+      //x <= 'h00; y <= 'h00; c_in <= 1;    // sum = 'h01
+      //#5 x <= 'h80; y <= 'h7F; c_in <= 1; // sum = 'h100
+      //#5 x <= 'hA0; y <= 'h0B; c_in <= 0; // sum = 'hAB
+      //#5 x <= 'h08; y <= 'h18; c_in <= 0; // sum = 'h20
+      //#5 x <= 'h7F; y <= 'hA0; c_in <= 0; // sum = 'h11F
       #5 $finish;
     end
 endmodule // Top
