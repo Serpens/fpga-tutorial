2dbae2d7a0202923d09497d4320679d706858f05
Albert Bogdanowicz <albertbogdanowicz@albertbogdanowicz.pl>
2019-01-26 12:11:30 +0100

    Completed data flip-flop

--- verilog/dff.v
+++ verilog/dff.v
@@ -1,11 +1,15 @@
 module dff(input wire clk,
            input wire data,
+	   input wire store,
            output wire out);
   reg stored;
   // You can also just declare "output reg out" above.
   assign out = stored;
 
   always @(posedge clk) begin
-    stored <= data;
+    // stored <= store ? data : stored
+    if (store) begin
+      stored <= data;
+    end 
   end
 endmodule
--- verilog/dff_tb.v
+++ verilog/dff_tb.v
@@ -3,20 +3,28 @@
 module Top;
   reg clk;
   reg data;
+  reg store;
   wire out;
 
-  dff dff0(clk, data, out);
+  dff dff0(clk, data, store, out);
 
   initial
     begin
-      $monitor($time, " clk = %b, data = %b, out = %b",
-               clk, data, out);
+      $monitor($time, " clk = %b, data = %b, stored = %b, out = %b",
+               clk, data, dff0.stored, out);
       $dumpfile(`VCD_FILE);
       $dumpvars;
 
       // Your test code here:
       clk <= 0; data <= 0;
-      // TODO
+      # 1 clk <= 1; data <= 0; store <= 0;
+      # 1 clk <= 0; data <= 1; store <= 1;
+      # 1 clk <= 1; data <= 0; store <= 1; 
+      # 1 clk <= 0; data <= 1; store <= 1;
+      # 1 clk <= 1; data <= 1; store <= 1;
+      # 1 clk <= 1; data <= 1; store <= 1;
+      # 1 clk <= 0; data <= 1; store <= 0; 
+      # 1 clk <= 1; data <= 0; store <= 0;
 
       #2 $finish;
     end
